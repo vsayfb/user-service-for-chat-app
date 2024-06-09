e50af12e12666107a028230b50c831ccf5ee3cb6
vsay <96926401+vsayfb@users.noreply.github.com>
2024-06-09 08:46:46 -0400

    move mongo-related annotations to MongoConfig

--- src/main/java/com/example/user_service/config/MongoConfig.java
+++ src/main/java/com/example/user_service/config/MongoConfig.java
@@ -2,10 +2,14 @@ package com.example.user_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @Configuration
+@EnableMongoRepositories(basePackages = "com.example.user_service.repository")
+@EnableMongoAuditing
 public class MongoConfig {
 
     @Bean
